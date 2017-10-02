b3a40c1038040bcc67eebc81243dd5d2d46c3b31
Piotr Szczepanik <piotrszczepanik29@wp.pl>
2017-10-02 21:57:08 +0200

    pliki

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = prompt('Dane a');
+var h = prompt('Dane h');
+var triangleArea = a*h/2;
+alert('Pole trójk¹ta  '+triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
